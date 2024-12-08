48681ae1365a2e3afae07b03708f6dafb25be71b
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-12-07 19:27:46 -0800

    deal with small number

--- src/lib/formatNumber.ts
+++ src/lib/formatNumber.ts
@@ -7,8 +7,12 @@ export const formatNumber = (number: number, numOfDecimals: number = 1) => {
     return (number / 1e6).toFixed(numOfDecimals) + "M";
   } else if (number >= 1e3) {
     return (number / 1e3).toFixed(numOfDecimals) + "K";
-  } else if (number < 1e-3) {
-    return number.toFixed(5).toString();
+  } else if (number < 9e-9) {
+    return number.toFixed(12).toString();
+  } else if (number < 9e-6) {
+    return number.toFixed(9).toString();
+  } else if (number < 9e-3) {
+    return number.toFixed(6).toString();
   } else {
     return number.toFixed(numOfDecimals).toString();
   }
