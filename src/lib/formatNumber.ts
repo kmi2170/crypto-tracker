e4214a1f319b009d529420af450e0cfd777c6612
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-12-08 01:46:34 -0800

    use scientific notation for small digit numbers

--- src/lib/formatNumber.ts
+++ src/lib/formatNumber.ts
@@ -8,11 +8,11 @@ export const formatNumber = (number: number, numOfDecimals: number = 1) => {
   } else if (number >= 1e3) {
     return (number / 1e3).toFixed(numOfDecimals) + "K";
   } else if (number < 9e-9) {
-    return number.toFixed(12).toString();
+    return number.toExponential(numOfDecimals);
   } else if (number < 9e-6) {
-    return number.toFixed(9).toString();
+    return number.toExponential(numOfDecimals);
   } else if (number < 9e-3) {
-    return number.toFixed(6).toString();
+    return number.toExponential(numOfDecimals);
   } else {
     return number.toFixed(numOfDecimals).toString();
   }
