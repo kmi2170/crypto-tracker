d27b96852377a3866a838e95c7fb8b5438bf1aaa
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-12-11 17:35:24 -0800

    define font, color

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,20 +1,33 @@
 "use client";
 
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import { Inter } from "next/font/google";
 
-// Create a theme instance.
+const inter = Inter({ subsets: ["latin"] });
+
+// const roboto = Roboto({
+//   weight: "400",
+//   subsets: ["latin"],
+// });
+
 let theme = createTheme({
   palette: {
-    // mode: "dark",
-    // primary: {
-    //   main: "#556cd6",
-    // },
-    // secondary: {
-    //   main: "#19857b",
-    // },
+    background: {
+      default: "rgba(255,248,220,0.25)",
+    },
+    primary: {
+      main: "rgba(0,65,106,0.8)",
+    },
+    secondary: {
+      main: "rgb(255,215,0)",
+      light: "rgb(255,192,203)",
+    },
     // error: {
     //   main: red.A400,
     // },
+  },
+  typography: {
+    fontFamily: inter.style.fontFamily,
   },
 });
 
