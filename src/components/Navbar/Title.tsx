5d3dbcb8a416a34e8ffff5ba9728938590c5b6a7
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-12-11 20:47:44 -0800

    make Title as a separate component; use sx props in idex

---
+++ src/components/Navbar/Title.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { memo } from "react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+import Typography, { TypographyProps } from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+
+const TitleWrapper = styled(Typography)<TypographyProps>({
+  "& a": {
+    color: "gold",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+});
+
+const Title = memo(() => {
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+
+  return (
+    <TitleWrapper
+      variant="h4"
+      component="h1"
+      sx={(theme) => ({
+        [theme.breakpoints.down("sm")]: {
+          fontSize: "1.25rem",
+        },
+      })}
+    >
+      <Link href={`/?${params.toString()}`}>Crypto Tracker</Link>
+    </TitleWrapper>
+  );
+});
+
+export default Title;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,73 +1,41 @@
-"use client";
-
-import Link from "next/link";
-import { useRouter, usePathname, useSearchParams } from "next/navigation";
-
 import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
-import Typography, { TypographyProps } from "@mui/material/Typography";
-import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 
 import SelectCurrency from "./SelectCurrency";
 import WatchListSidebar from "./WatchListSidebar";
-import { memo } from "react";
-
-const Wrapper = styled(Container)({
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "space-between",
-});
-
-const CurrencyAndWatchListWrapper = styled("div")<TypographyProps>({
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "center",
-  alignItems: "center",
-  gap: "1.0rem",
-});
+import Title from "./Title";
 
 const Navbar = () => {
   return (
     <AppBar position="sticky" sx={{ backgroundColor: "rgba(0,65,106,0.8)" }}>
       <Toolbar>
-        <Wrapper maxWidth="lg">
+        <Container
+          maxWidth="lg"
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
           <Title />
-          <CurrencyAndWatchListWrapper>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "1.0rem",
+            }}
+          >
             <SelectCurrency />
             <WatchListSidebar />
-          </CurrencyAndWatchListWrapper>
-        </Wrapper>
+          </Box>
+        </Container>
       </Toolbar>
     </AppBar>
   );
 };
 
 export default Navbar;
-
-const TitleWrapper = styled(Typography)<TypographyProps>({
-  "& a": {
-    color: "gold",
-    fontWeight: "bold",
-    cursor: "pointer",
-  },
-});
-
-const Title = memo(() => {
-  const searchParams = useSearchParams();
-  const params = new URLSearchParams(searchParams);
-
-  return (
-    <TitleWrapper
-      variant="h4"
-      component="h1"
-      sx={(theme) => ({
-        [theme.breakpoints.down("sm")]: {
-          fontSize: "1.25rem",
-        },
-      })}
-    >
-      <Link href={`/?${params.toString()}`}>Crypto Tracker</Link>
-    </TitleWrapper>
-  );
-});
