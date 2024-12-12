aa01f911168c13999c3dd14a68e6640798149f2c
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-12-11 17:35:53 -0800

    code clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,25 +15,3 @@ const Home = () => {
 };
 
 export default Home;
-
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   const currency = "USD";
-//   const queryClient = new QueryClient();
-
-//   await queryClient.prefetchQuery(["coins", currency], () =>
-//     fetchCoins(currency)
-//   );
-
-//   await queryClient.prefetchQuery(["trending", currency], () =>
-//     fetchTrendCoins(currency)
-//   );
-
-//   const _dehydrate = dehydrate(queryClient);
-//   console.log(_dehydrate?.queries?.[0]?.state);
-
-//   return {
-//     props: {
-//       dehydrateState: _dehydrate || null,
-//     },
-//   };
-// };
