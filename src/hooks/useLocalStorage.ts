5fcefdae7da33cd96f4c2c3de984b0de527ed16a
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-12-13 01:58:19 -0800

    add T to getItem

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -5,7 +5,7 @@ import { useCallback } from "react";
 export default function useLocalStorage<T>(key: string, initValue: T) {
   const getItemFromLocalStorage = () => {
     const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initValue;
+    return storedValue ? (JSON.parse(storedValue) as T) : initValue;
   };
 
   const setItemToLocalStorage = useCallback((value: T) => {
